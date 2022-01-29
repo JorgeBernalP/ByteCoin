6cdbd0db6764de582ee459bbb1e68e7be1d5c9e3
Jorge Bernal <jorgeb@neumomed.com>
2022-01-29 11:22:31 -0500

    First attempt at constrains

--- ByteCoin/ViewController.swift
+++ ByteCoin/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import NeumorphismKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var cryptoButton: NeumorphismButton!
+    @IBOutlet weak var currencyButton: NeumorphismButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        //Description Text
+        descriptionLabel.text = "Check the current prices of cryptocurrency arround the world in Colombian Peso."
+    
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '12.0'
+
+target 'ByteCoin' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ByteCoin
+  
+  pod 'NeumorphismKit', '~> 1.0'
+
+end
